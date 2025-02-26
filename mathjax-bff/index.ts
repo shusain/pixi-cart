97bb1d197f5868fd62b3c0eb3da772d57d8e0a3a
Shaun Husain <shaun.husain@gmail.com>
2025-02-26 02:48:09 -0600

    Added backend for preprocessing the functions to SVG

---
+++ mathjax-bff/index.ts
@@ -0,0 +1,47 @@
+import { mathjax } from 'mathjax-full/js/mathjax'
+// import { MathML} from 'mathjax-full/js/input/mathml'
+import { TeX } from 'mathjax-full/js/input/tex'
+import { SVG } from 'mathjax-full/js/output/svg'
+import { liteAdaptor } from 'mathjax-full/js/adaptors/liteAdaptor'
+import { RegisterHTMLHandler } from 'mathjax-full/js/handlers/html'
+import express from 'express';
+import cors from 'cors'
+
+const adaptor = liteAdaptor()
+RegisterHTMLHandler(adaptor)
+
+const mathjax_document = mathjax.document('', {
+  InputJax: new TeX(),
+  OutputJax: new SVG({ fontCache: 'none' })
+})
+
+const mathjax_options = {
+  em: 16,
+  ex: 8,
+  containerWidth: 1280
+}
+
+export function get_mathjax_svg(math: string): string {
+  const node = mathjax_document.convert(math, mathjax_options)
+  return adaptor.innerHTML(node)
+}
+
+console.log(get_mathjax_svg('\\frac{a}{1-a^2}'))
+
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+app.get('/tex2svg.svg', (req, res) => {
+  res.setHeader('Content-Type', 'image/svg+xml')
+  let svgString = get_mathjax_svg(req.query.tex as string)
+  // console.log(svgString)
+  console.log(svgString.replaceAll("currentColor","blue"))
+  res.send(svgString.replaceAll("currentColor","blue"))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
